a811141b1859d8e0a950821d07564770447f3b1b
Amir Berenji <a.berenji62@gmail.com>
2026-01-31 20:39:19 +0400

    product image

--- app/(main)/product-photography/page.tsx
+++ app/(main)/product-photography/page.tsx
@@ -4,25 +4,29 @@ import MasonryGallery from "../components/masonarygallery";
 
 export default function ProductPhotography() {
   const [images, setImages] = useState(() => [
-   "/assets/images/street-photography/street-photography (1)",
-"/assets/images/street-photography/street-photography (2)",
-"/assets/images/street-photography/street-photography (3)",
-"/assets/images/street-photography/street-photography (4)",
-"/assets/images/street-photography/street-photography (5)",
-"/assets/images/street-photography/street-photography (6)",
-"/assets/images/street-photography/street-photography (4)",
-"/assets/images/street-photography/street-photography (8)",
-"/assets/images/street-photography/street-photography (9)",
-"/assets/images/street-photography/street-photography (10)",
-"/assets/images/street-photography/street-photography (11)",
-"/assets/images/street-photography/street-photography (12)",
+"/assets/images/product/product (1).jpg",
+"/assets/images/product/product (2).jpg",
+"/assets/images/product/product (3).jpg",
+"/assets/images/product/product (4).jpg",
+"/assets/images/product/product (5).jpg",
+"/assets/images/product/product (6).jpg",
+"/assets/images/product/product (8).jpg",
+"/assets/images/product/product (9).jpg",
+"/assets/images/product/product (10).jpg",
+"/assets/images/product/product (11).jpg",
+"/assets/images/product/product (12).jpg",
+"/assets/images/product/product (13).jpg",
+"/assets/images/product/product (14).jpg",
+"/assets/images/product/product (15).jpg",
+"/assets/images/product/product (16).jpg",
+"/assets/images/product/product (17).jpg",
   ]);
 
   return (
     <>
       <MasonryGallery
         imagelist={images}
-        topimage="/assets/images/ads.jpeg"
+        topimage="/assets/images/product.jpeg"
       />
     </>
   );
