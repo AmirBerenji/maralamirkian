0b1b62eacedf0cda4fe5b5362a8650a2eff802e8
Amir Berenji <a.berenji62@gmail.com>
2026-01-31 21:07:39 +0400

    Create link and ads image

--- app/(main)/ads-and-events/page.tsx
+++ app/(main)/ads-and-events/page.tsx
@@ -4,23 +4,43 @@ import MasonryGallery from "../components/masonarygallery";
 
 export default function AdsAndEvents() {
   const [images, setImages] = useState(() => [
-    "/assets/images/product.jpeg",
-    "/assets/images/ads.jpeg",
-    "/assets/images/joker.jpeg",
-    "/assets/images/property.jpg",
-    "/assets/images/skate-man.jpeg",
-    "/assets/images/product.jpeg",
-    "/assets/images/ads.jpeg",
-    "/assets/images/joker.jpeg",
-    "/assets/images/property.jpg",
-    "/assets/images/skate-man.jpeg",
+    "/assets/images/events/events (1).jpg",
+"/assets/images/events/events (2).jpg",
+"/assets/images/events/events (3).jpg",
+"/assets/images/events/events (4).jpg",
+"/assets/images/events/events (5).jpg",
+"/assets/images/events/events (6).jpg",
+"/assets/images/events/events (8).jpg",
+"/assets/images/events/events (9).jpg",
+"/assets/images/events/events (10).jpg",
+"/assets/images/events/events (11).jpg",
+"/assets/images/events/events (12).jpg",
+"/assets/images/events/events (13).jpg",
+"/assets/images/events/events (14).jpg",
+"/assets/images/events/events (15).jpg",
+"/assets/images/events/events (16).jpg",
+"/assets/images/events/events (17).jpg",
+"/assets/images/events/events (18).jpg",
+"/assets/images/events/events (19).jpg",
+"/assets/images/events/events (20).jpg",
+"/assets/images/events/events (21).jpg",
+"/assets/images/events/events (22).jpg",
+"/assets/images/events/events (23).jpg",
+"/assets/images/events/events (24).jpg",
+"/assets/images/events/events (25).jpg",
+"/assets/images/events/events (26).jpg",
+"/assets/images/events/events (27).jpg",
+"/assets/images/events/events (28).jpg",
+"/assets/images/events/events (29).jpg",
+"/assets/images/events/events (30).jpg",
+"/assets/images/events/events (31).jpg",
   ]);
 
   return (
     <>
       <MasonryGallery
         imagelist={images}
-        topimage="/assets/images/property.jpg"
+        topimage="/assets/images/ads.jpeg"
       />
     </>
   );
